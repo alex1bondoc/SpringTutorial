eb463d2d79a5fe8ed647c550fe9539ac18b1d8ca
alex1bondoc <alex1bondoc@gmail.com>
2026-03-27 16:29:43 +0200

    First endpoint

---
+++ demo/src/main/java/com/alex/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.alex.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/testing")
+    public String test() {
+        return "Merge";
+    }
+}
