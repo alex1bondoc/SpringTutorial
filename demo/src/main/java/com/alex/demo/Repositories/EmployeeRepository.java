bfc70ab4572b4f1f8634ef1211ae875199ffc886
alex1bondoc <alex1bondoc@gmail.com>
2026-03-27 17:15:57 +0200

    Repository

--- demo/src/main/java/com/alex/demo/Repositories/EmployeeRepository.java
+++ demo/src/main/java/com/alex/demo/Repositories/EmployeeRepository.java
@@ -2,3 +2,10 @@ package com.alex.demo.Repositories;
 
 import com.alex.demo.Entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByJob(String job);
+}
